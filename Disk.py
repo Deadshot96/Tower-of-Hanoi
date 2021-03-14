b89b172e2bf7dd65a87680e68887fc3ee060a21f
Bhushan <bhushan1810patil@gmail.com>
2021-03-15 00:00:11 +0530

    added some functions to Disk.py

--- Disk.py
+++ Disk.py
@@ -1,8 +1,41 @@
 import pygame
+from pygame import Surface
 from settings import *
 from colors import *
+from Tower import Tower
 
 class Disk:
 
-    def __init__(self):
+    def __init__(self, index: int, tower: Tower):
+        self.index = index
+        self.tower = tower
+        self.width = index * 30
+        self.height = DISK_HEIGHT
+        self.color = DISKS_COLORS [index]
+        self.x = 0
+        self.y = 0
+
+        self.set_positions()
+
+    def set_positions(self):
+        y = self.tower.get_stack_height()
+        x = self.tower.get_x()
+
+        self.x = x - self.width // 2
+        self.y = y - self.height
+
+    def get_top_left(self):
         pass
+
+    def get_tower(self):
+        return self.tower
+
+    def set_tower(self, tower: Tower):
+        self.tower = tower
+        self.set_positions()
+
+    def draw(self, win: Surface):
+        pass
+
+    def get_index(self) -> int:
+        return self.index
